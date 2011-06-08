7ad2ee996a16cd9b47b579f8f9e06ca636300d29
soos <aleksander.usov@gmail.com>
2011-06-08 10:22:44 +0400

    -

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/python3.2
+##﻿!/usr/bin/env python3.2
 # -*- coding: utf-8 -*-
 
 import socket, ssl, sys
@@ -12,38 +13,41 @@ class hnh_server:
 		ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
 		ctx.options |= ssl.OP_NO_SSLv2
 		try:
-			ctx.load_cert_chain(certfile="authsrv.pem")
+			ctx.load_cert_chain(certfile="crt.pem",keyfile="key.pem")
 		except:
 			print("error: {} {}".format(sys.exc_info()[1],sys.exc_info()[2]))
 			return
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.bind(('', self.ssl_port))
 		s.listen(5)
-		while True:
-			newsocket, fromaddr = s.accept()
-			print('connected: ', fromaddr)
-			ss = ctx.wrap_socket(newsocket, server_side=True)
-			try:
-				while True:
-					data = ss.recv(1024)
-					if not data:
-						break
-					print(data)
-			finally:
-				connstream.shutdown(socket.SHUT_RDWR)
-				connstream.close()
+		#while True:
 		
-		# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		# ss = ssl.wrap_socket(sock=s, server_side=True)
-		# ss.listen(1)
-		# conn, addr = ss.accept()
-		# print('Connected: ', addr)
-		# while True:
-			# data = conn.recv(1024)
-			# if not data: break
-			# print(data)
-			# conn.send(data)
-		# conn.close()
+		newsocket, fromaddr = s.accept()
+		print('connected: ', fromaddr)
+		ss = ctx.wrap_socket(newsocket, server_side=True)
+		try:
+			data = ss.recv(1024)
+			if not data:
+				#TODO throw exception
+				pass
+			print(data)
+			ss.send(b'\x00\x00')
+			data = ss.recv(1024)
+			if not data:
+				#TODO throw exception
+				pass
+			print(data)
+			ss.send(b'\x00\x08deadbeef')
+			print('auth passed')
+		finally:
+			ss.shutdown(socket.SHUT_RDWR)
+			ss.close()
+			
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.bind(('', self.udp_port))
+		s.listen(5)
+		data = ss.recv(1024)
+		
 
 srv = hnh_server(1871, 1870)
 srv.start()
