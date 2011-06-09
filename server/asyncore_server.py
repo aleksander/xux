da5d027ba9d73426724faadbee736eaf34b62e82
Aleksander Usov <aleksander.usov@gmail.com>
2011-06-09 12:46:16 +0400

    -

--- server/asyncore_server.py
+++ server/asyncore_server.py
@@ -2,42 +2,58 @@
 # -*- coding: utf-8 -*-
 
 import asyncore
+import select
 import socket
 import ssl
 
 class AuthHandler(asyncore.dispatcher_with_send):
-	def __init__(self, conn):
-        asyncore.dispatcher_with_send.__init__(self, conn)
-        self.socket = ssl.wrap_socket(conn, server_side=True, certfile='keycert.pem', do_handshake_on_connect=False)
-        while True:
-            try:
-                self.socket.do_handshake()
-                break
-            except ssl.SSLError, err:
-                if err.args[0] == ssl.SSL_ERROR_WANT_READ:
-                    select.select([self.socket], [], [])
-                elif err.args[0] == ssl.SSL_ERROR_WANT_WRITE:
-                    select.select([], [self.socket], [])
-                else:
-                    raise
-		
+	def __init__(self, sock, ctx):
+		asyncore.dispatcher_with_send.__init__(self, sock)
+		self.sock = ctx.wrap_socket(sock, server_side=True, do_handshake_on_connect=False)
+		while True:
+			try:
+				self.sock.do_handshake()
+				break
+			except ssl.SSLError as err:
+				if err.args[0] == ssl.SSL_ERROR_WANT_READ:
+					select.select([self.sock], [], [])
+				elif err.args[0] == ssl.SSL_ERROR_WANT_WRITE:
+					select.select([], [self.sock], [])
+				else:
+					raise
+		self.state = 0
+
+	def readable(self):
+		if isinstance(self.sock, ssl.SSLSocket):
+			while self.sock.pending() > 0:
+				self.handle_read_event()
+		return True
+
+	def handle_read(self):
 		try:
-			data = ss.recv(1024)
-			if not data:
-				#TODO throw exception
-				pass
+			data = self.sock.recv(1024)
+			# if not data:
+				# # TODO throw exception
+				# pass
 			print(data)
-			ss.send(b'\x00\x00')
-			data = ss.recv(1024)
-			if not data:
-				#TODO throw exception
-				pass
-			print(data)
-			ss.send(b'\x00\x08deadbeef')
-			print('auth passed')
-		finally:
-			ss.shutdown(socket.SHUT_RDWR)
-			ss.close()
+			if self.state == 0:
+				self.sock.send(b'\x00\x00')
+				self.state = 1
+			elif self.state == 1:
+				self.sock.send(b'\x00\x08deadbeef')
+				print('auth passed')
+				self.state = 2
+			# else:
+				# self.sock.shutdown(socket.SHUT_RDWR)
+				# self.sock.close()
+		except:
+			print('connection lost')
+			self.sock.shutdown(socket.SHUT_RDWR)
+			self.sock.close()
+
+
+	def handle_error(self):
+		raise
 
 class AuthServer(asyncore.dispatcher):
 	def __init__(self, host, port):
@@ -54,7 +70,7 @@ class AuthServer(asyncore.dispatcher):
 
 	def handle_accepted(self, sock, addr):
 		print('accepted: ',repr(addr))
-		handler = AuthHandler(sock)
+		handler = AuthHandler(sock, self.ctx)
 
 	def handle_error(self):
 		raise
