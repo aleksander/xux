92b89d7973b396e51364e4c427afeec4d16947a6
Aleksander Usov <aleksander.usov@gmail.com>
2011-06-17 18:37:53 +0400

    random tilemap generation using instances

--- client/panda3d_test.py
+++ client/panda3d_test.py
@@ -1,65 +1,35 @@
-from math import pi, sin, cos
+import direct.directbase.DirectStart
+from pandac.PandaModules import *
+from direct.interval.IntervalGlobal import *
+from direct.task.Task import Task
+import random
 
-from direct.showbase.ShowBase import ShowBase
-from direct.task import Task
-from direct.actor.Actor import Actor
-from direct.interval.IntervalGlobal import Sequence
-from panda3d.core import Point3
-from direct.gui.DirectGui import *
-#import direct.directbase.DirectStart
+tiles = loader.loadModel("2.egg")
+tile1 = tiles.find('**/grass')
+tile2 = tiles.find('**/Plane')
+tile3 = tiles.find('**/Plane_001')
+tile4 = tiles.find('**/Plane_002')
+#tiles.ls()
+tile1.setPos(.0,.0,.0)
+tile2.setPos(.0,.0,.0)
+tile3.setPos(.0,.0,.0)
+tile4.setPos(.0,.0,.0)
+tiles = [tile1,tile2,tile3,tile4]
+terrain = NodePath('terrain')
+terrain.reparentTo(render)
+for x in xrange(-50,50):
+	for y in xrange(-50,50):
+		tile = terrain.attachNewNode('tile')
+		tile.setPos(x,y,0)
+		random.choice(tiles).instanceTo(tile)
+		# tile = random.choice(tiles)
+		# tile = tile.copyTo(terrain)
+		# tile.setPos(x*1.1,y*1.1,0)
+		#terrain.flattenStrong()
 
-class MyApp(ShowBase):
+base.disableMouse()
+camera.setPos(200,200,200)
+camera.lookAt(terrain)
+base.enableMouse()
 
-	def __init__(self):
-		ShowBase.__init__(self)
-		
-		# Load the environment model.
-		self.environ = self.loader.loadModel("models/environment")
-		# Reparent the model to render.
-		self.environ.reparentTo(self.render)
-		# Apply scale and position transforms on the model.
-		self.environ.setScale(0.25, 0.25, 0.25)
-		self.environ.setPos(-8, 42, 0)
-		# Add the spinCameraTask procedure to the task manager.
-		self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
-
-		# Load and transform the panda actor.
-		self.pandaActor = Actor("models/panda-model", {"walk": "models/panda-walk4"})
-		self.pandaActor.setScale(0.005, 0.005, 0.005)
-		self.pandaActor.reparentTo(self.render)
-		# Loop its animation.
-		self.pandaActor.loop("walk")
-
-		# Create the four lerp intervals needed for the panda to walk back and forth.
-		pandaPosInterval1 = self.pandaActor.posInterval(13, Point3(0, -10, 0), startPos=Point3(0, 10, 0))
-		pandaPosInterval2 = self.pandaActor.posInterval(13, Point3(0, 10, 0), startPos=Point3(0, -10, 0))
-		pandaHprInterval1 = self.pandaActor.hprInterval(3, Point3(180, 0, 0), startHpr=Point3(0, 0, 0))
-		pandaHprInterval2 = self.pandaActor.hprInterval(3, Point3(0, 0, 0), startHpr=Point3(180, 0, 0))
-
-		# Create and play the sequence that coordinates the intervals.
-		self.pandaPace = Sequence(pandaPosInterval1, pandaHprInterval1, pandaPosInterval2, pandaHprInterval2, name="pandaPace")
-		self.pandaPace.loop()
-		
-		#GUI
-		DirectButton(text="(-.9, .0, .9)", scale=.06, pos=(-.9, .0, .9))
-		DirectButton(text="(.9, .0, .9)", scale=.06, pos=(.9, .0, .9))
-		DirectButton(text="(.9, .0, -.9)", scale=.06, pos=(.9, .0, -.9))
-		DirectButton(text="(-.9, .0, -.9)", scale=.06, pos=(-.9, .0, -.9))
-		
-		DirectLabel(text="User", scale=.06, pos=(-.16, .0, .2))
-		DirectEntry(scale=.06, pos=(.0, .0, .2), rolloverSound=None, clickSound=None)
-		DirectLabel(text="Password", scale=.06, pos=(-.16, .0, .1))
-		DirectEntry(scale=.06, pos=(.0, .0, .1), obscured=True, rolloverSound=None, clickSound=None)
-		DirectButton(text="Authorize", scale=.06, rolloverSound=None, clickSound=None)
-		
-	# Define a procedure to move the camera.
-	def spinCameraTask(self, task):
-		angleDegrees = task.time * 6.0
-		angleRadians = angleDegrees * (pi / 180.0)
-		self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
-		self.camera.setHpr(angleDegrees, 0, 0)
-		return Task.cont
-
-app = MyApp()
-app.run()
-#run()
+run()
