fd7918a79568d421a28d7e0e67964752c84a63aa
Aleksander Usov <aleksander.usov@gmail.com>
2011-11-10 15:04:12 +0300

    debug printing replaced with logging. some optimizations. resources parsing and saving.

--- client/client.py
+++ client/client.py
@@ -4,71 +4,12 @@ from panda3d.core import *
 from direct.distributed import PyDatagram, PyDatagramIterator
 from direct.task import *
 from direct.interval.IntervalGlobal import *
+import logging
 
-############################################################################
 
 def dbg(data):
-	print data
+	logging.info(data)
 
-############################################################################
-
-class txQueue:
-	def __init__(self, conn, cwriter, addr):
-		self.conn = conn
-		self.cwriter = cwriter
-		self.addr = addr
-		self.que = [] # (seq, type, packet)
-		self.last_tx_time = -10.
-		
-	#def ack(self):
-	#	pass
-
-	def add_sess(self, user, cookie):
-		data = self.make_sess(user, cookie)
-		self.que.append((0, 0, data))
-
-	def remove_sess(self):
-		dbg("remove sess")
-		self.que = [i for i in self.que if i[1] != 0]
-		if len(self.que) > 0:
-			self.last_tx_time = -10
-		
-	#def add(self, seq, packet):
-	#	pass
-
-	def make_sess(self, user, cookie):
-		data = PyDatagram.PyDatagram()
-		data.addUint8(0) # SESS
-		data.addUint16(1) # ???
-		data.addZString(u'Haven') # protocol name
-		data.addUint16(2) # version
-		data.addZString(user)
-		data.appendData(cookie)
-		return data
-
-	def send_ask(self, seq):
-		data = PyDatagram.PyDatagram()
-		data.addUint8(2) # ACK
-		data.addUint16(seq)
-		self.cwriter.send(data, self.conn, self.addr)
-
-	def serve(self, time):
-		delta = time - self.last_tx_time
-		if delta > 1.0: #TODO - hardcoding
-			if len(self.que) > 0:
-				self.cwriter.send(self.que[0][2], self.conn, self.addr)
-				dbg("sent. delta={0}".format(delta))
-			else:
-				#TODO send BEAT
-				pass
-			self.last_tx_time = time
-		# send_current_request() until not current_datagram_acked()
-		#if current_request not acked():
-		#	if curren_time() - last_send_time > 0.1:
-		#		send(current_request)
-		#		last_send_time = current_time()
-		#if curren_time() - last_send_time > 1.:
-		#	send(BEAT)
 
 class hnh_client(ShowBase):
 	def __init__(self, host, ssl_port, udp_port):
@@ -79,8 +20,9 @@ class hnh_client(ShowBase):
 		self.addr.setHost(self.host, self.udp_port)
 		self.tiles = {}
 		self.widgets = []
-		#for i in range(0,256):
-		#	self.tiles.append('')
+		self.resources = {}
+		logging.basicConfig(filename='client.log', filemode="w", level=logging.INFO)
+		
 		self.sess_errors = {
 			0:'OK',
 			1:'AUTH',
@@ -159,7 +101,7 @@ class hnh_client(ShowBase):
 		#f = open('cookie','wb')
 		#f.write(self.cookie)
 		#f.close()
-		self.user_name = name
+		self.user = name
 		#dbg('cookie: '+self.cookie)
 		return True
 
@@ -180,8 +122,9 @@ class hnh_client(ShowBase):
 		self.creader.addConnection(self.conn)
 		taskMgr.add(self.rx_task, "rx_task")
 		taskMgr.add(self.tx_task, "tx_task")
-		self.tx_que = txQueue(self.conn, self.cwriter, self.addr)
-		self.tx_que.add_sess(self.user_name, self.cookie)
+		self.current_request = self.tx_sess
+		#TODO fsm.enter(sess)
+		self.last_tx_time = -10.
 		self.run()
 
 	def rx_task(self, task):
@@ -198,15 +141,23 @@ class hnh_client(ShowBase):
 		return task.cont
 
 	def tx_task(self, task):
-		self.tx_que.serve(task.time)
+		delta = task.time - self.last_tx_time
+		if self.current_request != None:
+			if delta > 1.0: #TODO - hardcoding
+				self.current_request()
+				self.last_tx_time = task.time
+		else:
+			if delta > 5.0:
+				self.tx_beat()
+				self.last_tx_time = task.time
 		return task.cont
 
 	def rx_sess(self, data):
 		error = data.getUint8()
 		if error == 0:
-			self.tx_que.remove_sess()
-		if error in sess_errors:
-			dbg('  error={0} ({1})'.format(error, sess_errors[error]))
+			self.current_request = None
+		if error in self.sess_errors:
+			dbg('  error={0} ({1})'.format(error, self.sess_errors[error]))
 		else:
 			dbg('  error={0} (unknown)'.format(error))
 
@@ -223,56 +174,47 @@ class hnh_client(ShowBase):
 				dbg('  seq={0} rel=UNKNOWN ({1}) len={2}'.format(seq, rel_type, rel_len))
 				data.skipBytes(rel_len)
 			else:
-				dbg('  seq={0} rel={0} len={2}'.format(seq, self.rel_types[rel_type][0], rel_len))
+				dbg('  seq={0} rel={1} len={2}'.format(seq, self.rel_types[rel_type][0], rel_len))
 				rel = data.extractBytes(rel_len)
 				dg = Datagram(rel)
 				pdi = PyDatagramIterator.PyDatagramIterator(dg)
 				self.rel_types[rel_type][1](pdi)
 			seq += 1
-		self.tx_que.send_ask(seq)
+		self.tx_ask(seq-1)
 
+	def rx_rel_parse_args(self, data):
+		args = []
+		while data.getRemainingSize():
+			arg_type = data.getUint8()
+			if arg_type == 0: # END
+				dbg('      END')
+				if data.getRemainingSize():
+					dbg('      DATA REMAINS')
+			elif arg_type == 1: # INT
+				args.append((arg_type, data.getInt32()))
+			elif arg_type == 2: # STR
+				args.append((arg_type, data.getZString()))
+			elif arg_type == 3: # COORD
+				args.append((arg_type, (data.getInt32(), data.getInt32())))
+			elif arg_type == 6: # COLOR
+				args.append((arg_type, (data.getUint8(), data.getUint8(), data.getUint8(), data.getUint8())))
+			else: # UNKNOWN LIST TYPE
+				break
+		return args
+	
 	def rx_rel_newwdg(self, data):
 		wdg_id = data.getUint16()
 		wdg_type = data.getZString()
 		wdg_coord = [data.getInt32(),data.getInt32()]
 		wdg_parent = data.getUint16()
-		self.new_widget(wdg_id, wdg_type, wdg_parent)
-		dbg('    id={0} type={1} coord={2} parent={3}'.format(wdg_id,wdg_type,wdg_coord,wdg_parent))
-		while data.getRemainingSize():
-			wdg_lt = data.getUint8()
-			if wdg_lt not in self.wdg_list_types:
-				dbg('     UNKNOWN LIST TYPE')
-				break
-			if wdg_lt == 0: # END
-				dbg('      END')
-			elif wdg_lt == 1: # INT
-				dbg('      {0}={1}'.format(self.wdg_list_types[wdg_lt], data.getInt32()))
-			elif wdg_lt == 2: # STR
-				dbg('      {0}={1}'.format(self.wdg_list_types[wdg_lt], data.getZString()))
-			elif wdg_lt == 3: # COORD
-				dbg('      {0}={1}'.format(self.wdg_list_types[wdg_lt], [data.getInt32(),data.getInt32()]))
-			elif wdg_lt == 6: # COLOR
-				dbg('      {0}={1}'.format(self.wdg_list_types[wdg_lt], [data.getUint8(),data.getUint8(),data.getUint8(),data.getUint8()]))
+		wdg_args = self.rx_rel_parse_args(data)
+		self.new_widget(wdg_id, wdg_type, wdg_coord, wdg_parent, wdg_args)
 
 	def rx_rel_wdgmsg(self, data):
 		wdg_id = data.getUint16()
 		wdg_msg_name = data.getZString()
-		dbg('    id={0} name={1}'.format(wdg_id, wdg_msg_name))
-		while data.getRemainingSize():
-			wdg_lt = data.getUint8()
-			if wdg_lt not in self.wdg_list_types:
-				dbg('     UNKNOWN LIST TYPE')
-				break
-			if wdg_lt == 0: # END
-				dbg('      END')
-			elif wdg_lt == 1: # INT
-				dbg('      {0}={1}'.format(self.wdg_list_types[wdg_lt], data.getInt32()))
-			elif wdg_lt == 2: # STR
-				dbg('      {0}={1}'.format(self.wdg_list_types[wdg_lt], data.getZString()))
-			elif wdg_lt == 3: # COORD
-				dbg('      {0}={1}'.format(self.wdg_list_types[wdg_lt], [data.getInt32(),data.getInt32()]))
-			elif wdg_lt == 6: # COLOR
-				dbg('      {0}={1}'.format(self.wdg_list_types[wdg_lt], [data.getUint8(),data.getUint8(),data.getUint8(),data.getUint8()]))
+		wdg_args = self.rx_rel_parse_args(data)
+		self.widget_message(wdg_id, wdg_msg_name, wdg_args)
 
 	def rx_rel_dstwdg(self, data):
 		wdg_id = data.getUint16()
@@ -289,7 +231,11 @@ class hnh_client(ShowBase):
 		pass
 
 	def rx_rel_resid(self, data):
-		pass
+		res_id = data.getUint16()
+		res_name = data.getZString()
+		res_ver = data.getUint16()
+		self.resources[res_id] = (res_name, res_ver)
+		# dbg('    id={0} name={1} ver={2}'.format(res_id, res_name, res_ver))
 
 	def rx_rel_party(self, data):
 		pass
@@ -335,15 +281,49 @@ class hnh_client(ShowBase):
 	def rx_close(self, data):
 		pass
 
+	def tx_sess(self):
+		data = PyDatagram.PyDatagram()
+		data.addUint8(0) # SESS
+		data.addUint16(1) # ???
+		data.addZString(u'Haven') # protocol name
+		data.addUint16(2) # version
+		data.addZString(self.user)
+		data.appendData(self.cookie)
+		self.cwriter.send(data, self.conn, self.addr)
+		dbg("---> SESS")
+
 #	def tx_rel_wdgmsg(self, seq):
 #		data = PyDatagram.PyDatagram()
 #		data.addUint8(1) # REL
 #		data.addUint16(seq)
 #		self.cwriter.send(data, self.conn, self.addr)
-		
-	def new_widget(self, wdg_id, wdg_type, parent, args = []):
+
+	def tx_ask(self, seq):
+		data = PyDatagram.PyDatagram()
+		data.addUint8(2) # ACK
+		data.addUint16(seq)
+		self.cwriter.send(data, self.conn, self.addr)
+		dbg("---> ACK seq={0}".format(seq))
+
+	def tx_beat(self):
+		data = PyDatagram.PyDatagram()
+		data.addUint8(3) # BEAT
+		self.cwriter.send(data, self.conn, self.addr)
+		dbg("---> BEAT")
+
+	def new_widget(self, wdg_id, wdg_type, wdg_coord, wdg_parent, wdg_args):
 		# self.widgets[wdg_id] = (wdg_type, parent)
-		pass
+		dbg('    id={0} type={1} coord={2} parent={3}'.format(wdg_id, wdg_type, wdg_coord, wdg_parent))
+		for arg in wdg_args:
+			dbg('      {0}={1}'.format(self.wdg_list_types[arg[0]], arg[1]))
+
+	def widget_message(self, wdg_id, wdg_msg_name, wdg_args):
+		dbg('    id={0} name={1}'.format(wdg_id, wdg_msg_name))
+		for arg in wdg_args:
+			if wdg_msg_name == "add" and arg[0] == 1: # INT
+				dbg('      {0}={1} ({2})'.format(self.wdg_list_types[arg[0]], self.resources[arg[1]][0], self.resources[arg[1]][1]))
+			else:
+				dbg('      {0}={1}'.format(self.wdg_list_types[arg[0]], arg[1]))
 
 	def destroy_widget(self, wdg_id):
 		#TODO
