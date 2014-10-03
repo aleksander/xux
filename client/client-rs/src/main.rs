7be0e1b5b6b5506fefa42f865f05022d47de8fa4
Your Name <you@example.com>
2014-10-03 19:57:13 +0400

    WIP

--- client/client-rs/src/main.rs
+++ client/client-rs/src/main.rs
@@ -29,89 +29,82 @@ struct MyError {
     detail: Option<String>,
 }
 
-struct SalemClient;
+fn authorize (host: &str, port: u16, user: &str, pass: &str) -> Result<Vec<u8>, MyError> {
+    //let stream = TcpStream::connect(host, port).unwrap();
+    let stream = match TcpStream::connect(host, port) {
+        Ok(e)=>e,
+        Err(e)=>return Err(MyError{source:"connect", detail:e.detail})
+    };
+    //let stream = tryio!(TcpStream::connect(host, port));
 
-impl SalemClient {
+    let mut stream = SslStream::new(&SslContext::new(Sslv23).unwrap(), stream).unwrap();
 
-    //fn new () -> SalemClient { SalemClient }
-
-    fn authorize (self, host: &str, port: u16, user: &str, pass: &str) -> Result<Vec<u8>, MyError> {
-        //let stream = TcpStream::connect(host, port).unwrap();
-        let stream = match TcpStream::connect(host, port) {
-            Ok(e)=>e,
-            Err(e)=>return Err(MyError{source:"connect", detail:e.detail})
-        };
-        //let stream = tryio!(TcpStream::connect(host, port));
-
-        let mut stream = SslStream::new(&SslContext::new(Sslv23).unwrap(), stream).unwrap();
-
-        // send 'pw' command
-        // TODO form buffer and send all with one call
-        stream.write_be_u16((3+user.as_bytes().len()+1+32) as u16).unwrap();
-        stream.write("pw".as_bytes()).unwrap();
-        stream.write_u8(0u8).unwrap();
-        stream.write(user.as_bytes()).unwrap();
-        stream.write_u8(0u8).unwrap();
-        let pass_hash = hash(SHA256, pass.as_bytes());
-        assert!(pass_hash.len() == 32u);
-        stream.write(pass_hash.as_slice()).unwrap();
-        stream.flush().unwrap();
-        let length = stream.read_be_u16().ok().expect("read error");
-        let msg = stream.read_exact(length as uint).ok().expect("read error");
-        println!("msg='{}'", str::from_utf8(msg.as_slice()).unwrap());
-        //println!("msg='{}'", msg.as_slice().to_hex());
-        if msg.len() < "ok\0\0".len() {
-            return Err(MyError{source:"unexpected server answer", detail:Some(String::from_utf8(msg).unwrap())});
-        }
-
-        // send 'cookie' command
-        if (msg[0] == ('o' as u8)) && (msg[1] == ('k' as u8)) {
-            // TODO form buffer and send all with one call
-            stream.write_be_u16(("cookie".as_bytes().len()+1) as u16).unwrap();
-            stream.write("cookie".as_bytes()).unwrap();
-            stream.write_u8(0u8).unwrap();
-            stream.flush().unwrap();
-            let length = stream.read_be_u16().ok().expect("read error");
-            let msg = stream.read_exact(length as uint).ok().expect("read error");
-            //println!("msg='{}'", str::from_utf8(msg.as_slice()).unwrap());
-            println!("msg='{}'", msg.as_slice().to_hex());
-            //TODO check cookie length
-            return Ok(msg.slice_from(3).to_vec());
-        }
+    // send 'pw' command
+    // TODO form buffer and send all with one call
+    stream.write_be_u16((3+user.as_bytes().len()+1+32) as u16).unwrap();
+    stream.write("pw".as_bytes()).unwrap();
+    stream.write_u8(0u8).unwrap();
+    stream.write(user.as_bytes()).unwrap();
+    stream.write_u8(0u8).unwrap();
+    let pass_hash = hash(SHA256, pass.as_bytes());
+    assert!(pass_hash.len() == 32u);
+    stream.write(pass_hash.as_slice()).unwrap();
+    stream.flush().unwrap();
+    let length = stream.read_be_u16().ok().expect("read error");
+    let msg = stream.read_exact(length as uint).ok().expect("read error");
+    println!("msg='{}'", str::from_utf8(msg.as_slice()).unwrap());
+    //println!("msg='{}'", msg.as_slice().to_hex());
+    if msg.len() < "ok\0\0".len() {
         return Err(MyError{source:"unexpected server answer", detail:Some(String::from_utf8(msg).unwrap())});
     }
 
-    fn connect (self, cookie: &[u8]) {
-        use std::io::MemWriter;
-
-        //let addr = SocketAddr::from_str("game.salemthegame.com:1870");
-        let addr: SocketAddr = FromStr::from_str("0.0.0.0:0").unwrap();
-        let sock = UdpSocket::bind(addr).unwrap();
-        let addr: SocketAddr = FromStr::from_str("148.251.44.214:1870").unwrap();
-        let mut stream = sock.connect(addr);
-
-        //unknown=2 proto=Salem ver=33 user=soos cookie=[ .//J..%.....R...G......Q x![..e.b]
-        //let mut buf = Vec::new();
-        let mut w = MemWriter::new();
-        w.write_u8(0u8).unwrap();
-        w.write_le_u16(2u16).unwrap();
-        w.write("Salem".as_bytes()).unwrap();
-        w.write_u8(0u8).unwrap();
-        w.write_le_u16(34u16).unwrap();
-        w.write("salvian".as_bytes()).unwrap();
-        w.write_u8(0u8).unwrap();
-        w.write_le_u16(32u16).unwrap();
-        w.write(cookie).unwrap();
-        stream.write(w.unwrap().as_slice()).unwrap();
+    // send 'cookie' command
+    if (msg[0] == ('o' as u8)) && (msg[1] == ('k' as u8)) {
+        // TODO form buffer and send all with one call
+        stream.write_be_u16(("cookie".as_bytes().len()+1) as u16).unwrap();
+        stream.write("cookie".as_bytes()).unwrap();
+        stream.write_u8(0u8).unwrap();
         stream.flush().unwrap();
-
-        //let error = stream.read_u8().unwrap();
-        //println!("session error = {}", error);
-        let mut buf = [0u8, ..65535];
-        let len = stream.read(buf.as_mut_slice()).unwrap();
-        println!("result = {}", buf.slice_to(len).to_hex());
+        let length = stream.read_be_u16().ok().expect("read error");
+        let msg = stream.read_exact(length as uint).ok().expect("read error");
+        //println!("msg='{}'", str::from_utf8(msg.as_slice()).unwrap());
+        println!("msg='{}'", msg.as_slice().to_hex());
+        //TODO check cookie length
+        return Ok(msg.slice_from(3).to_vec());
     }
+    return Err(MyError{source:"unexpected server answer", detail:Some(String::from_utf8(msg).unwrap())});
+}
 
+fn sess (name: &str, cookie: &[u8]) -> Vec<u8> {
+    use std::io::MemWriter;
+
+    //let sock = UdpSocket::bind(addr).unwrap();
+    //let addr: SocketAddr = FromStr::from_str("148.251.44.214:1870").unwrap();
+    //let mut stream = sock.connect(addr);
+
+    //unknown=2 proto=Salem ver=33 user=soos cookie=[ .//J..%.....R...G......Q x![..e.b]
+    //let mut buf = Vec::new();
+    let mut w = MemWriter::new();
+    w.write_u8(0u8).unwrap();
+    w.write_le_u16(2u16).unwrap();
+    w.write("Salem".as_bytes()).unwrap();
+    w.write_u8(0u8).unwrap();
+    w.write_le_u16(34u16).unwrap();
+    w.write(name.as_bytes()).unwrap();
+    w.write_u8(0u8).unwrap();
+    w.write_le_u16(32u16).unwrap();
+    w.write(cookie).unwrap();
+    w.unwrap()
+
+    //stream.write(w.unwrap().as_slice()).unwrap();
+    //stream.flush().unwrap();
+
+    //let error = stream.read_u8().unwrap();
+    //println!("session error = {}", error);
+
+    //let mut buf = [0u8, ..65535];
+    //let len = stream.read(buf.as_mut_slice()).unwrap();
+    //println!("result = {}", buf.slice_to(len).to_hex());
 }
 
 
@@ -146,10 +139,9 @@ fn main() {
     let port: u16 = 1871;
     let user = "salvian";
     let pass = "простойпароль";
-    let client = SalemClient;
 
     println!("authorize at {}:{}", host, port);
-    let cookie = match client.authorize(host, port, user, pass) {
+    let cookie = match authorize(host, port, user, pass) {
         Ok(cookie) => cookie,
         Err(e) => { println!("error. {}: {}", e.source, e.detail.unwrap()); return; }
     };
@@ -159,31 +151,44 @@ fn main() {
 
 
 
-    let addr: SocketAddr = FromStr::from_str("127.0.0.1:65500").unwrap();
-    let mut _sock = UdpSocket::bind(addr).unwrap();
+    let server_addr: SocketAddr = FromStr::from_str("148.151.44.214:1870").unwrap();
+    let any_addr:    SocketAddr = FromStr::from_str("0.0.0.0:0").unwrap();
+    let mut udp_rx = UdpSocket::bind(any_addr).unwrap();
+    let mut udp_tx = udp_rx.clone();
+
+    // echo server
+    /* let mut server_sock = UdpSocket::bind(server_addr).unwrap();
+    spawn(proc() {
+        loop {
+            let mut buf = [0u8, ..128];
+            let (len, src) = server_sock.recv_from(buf).unwrap();
+            server_sock.send_to(buf.slice_to(len), src).unwrap();
+        }
+    }); */
+
+    let (main_tx, sender_rx) = channel();
+    let (sender_tx, main_rx) = channel();
 
     // UDP sender
-    let mut sock = _sock.clone();
     spawn(proc() {
-        sock.send_to("1. Hello, World!!!".as_bytes(), addr).unwrap();
-        sock.send_to("2. Hello, World!!!".as_bytes(), addr).unwrap();
-        sock.send_to("3. Hello, World!!!".as_bytes(), addr).unwrap();
-    });
-
-    // UDP receiver
-    let mut sock = _sock.clone();
-    spawn(proc() {
-        for _ in range(1u, 3) {
-            let mut buf = [0u8, ..128];
-            let (len,_) = sock.recv_from(buf.as_mut_slice()).unwrap();
-            println!("seceive: {}", buf.slice_to(len).to_hex());
+        loop {
+            let buf: Vec<u8> = sender_rx.recv();
+            udp_tx.send_to(buf.as_slice(), server_addr).unwrap();
         }
     });
 
+    // UDP receiver
+    spawn(proc() {
+        loop {
+            let mut buf = [0u8, ..128];
+            let (len,_) = udp_rx.recv_from(buf.as_mut_slice()).unwrap();
+            println!("seceive: {}", buf.slice_to(len).to_hex());
+            sender_tx.send(());
+        }
+    });
 
-
-
-    client.connect(cookie.as_slice());
+    main_tx.send(sess(user.as_slice(), cookie.as_slice()));
+    main_rx.recv();
 }
 
 
