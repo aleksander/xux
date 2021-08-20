c9b9598fb0ccbdf357f142525550e6522d38e03e
Aleksander Usov <aleksander.usov@gmail.com>
2021-08-21 00:44:00 +0300

    rustfmt

--- client-macroquad/src/main.rs
+++ client-macroquad/src/main.rs
@@ -1,39 +1,30 @@
 #![feature(stmt_expr_attributes)]
 
+use anyhow::anyhow;
+use log::{debug, error, info, warn};
+use macroquad::prelude::{
+    clear_background, is_mouse_button_down, is_mouse_button_pressed, is_quit_requested, mouse_position, mouse_wheel, next_frame, prevent_quit, screen_height, screen_width, set_camera, vec2, Camera2D,
+    Color, DrawTextureParams, FilterMode, MouseButton, Rect, Vec2, BLACK, BLUE, WHITE,
+};
+use ron::de::from_reader;
 use std::{
     collections::BTreeMap,
-    sync::mpsc::{
-        Sender,
-        Receiver,
-        TryRecvError::*,
-    },
-    io::BufReader,
-    fs::File,
     default::Default,
+    fs::File,
+    io::BufReader,
+    sync::mpsc::{Receiver, Sender, TryRecvError::*},
 };
-use macroquad::prelude::{prevent_quit, clear_background, next_frame, BLACK, WHITE, BLUE, Vec2, Camera2D, mouse_wheel, mouse_position, FilterMode, MouseButton, vec2, is_mouse_button_down, is_mouse_button_pressed, Rect, screen_width, screen_height, is_quit_requested, set_camera, DrawTextureParams, Color};
-use xux::{
-    Result,
-    client,
-    driver,
-    state,
-    proto::{ResID, ObjID, ObjXY},
-};
-use anyhow::anyhow;
-use log::{error, info, warn, debug};
-use ron::de::from_reader;
-use xux::state::{Surface, WdgID};
 use xux::state::Event::Obj;
+use xux::state::{Surface, WdgID};
+use xux::{
+    client, driver,
+    proto::{ObjID, ObjXY, ResID},
+    state, Result,
+};
 
 #[macroquad::main("2d-macroquad-egui")]
-async fn main () -> Result<()> {
-
-    env_logger::builder()
-        .format_target(false)
-        .format_module_path(false)
-        .format_level(true)
-        .format_timestamp(None)
-        .init();
+async fn main() -> Result<()> {
+    env_logger::builder().format_target(false).format_module_path(false).format_level(true).format_timestamp(None).init();
 
     info!("Starting...");
 
@@ -46,32 +37,26 @@ async fn main () -> Result<()> {
 
     let username = args[1].clone();
     let password = args[2].clone();
-
     let host = "game.havenandhearth.com";
-
     let auth_port = 1871;
     let game_port = 1870;
 
     //TODO take all authorisation information from the GUI (maybe cache values in .config after the user first time enters them)
 
     let (login, cookie) = client::authorize(host, auth_port, username, password)?;
-
     let (ll_event_tx, hl_event_rx) = client::run_threaded(host, game_port, login, cookie)?;
-
     let mut render_ctx = RenderContext::new(ll_event_tx, hl_event_rx);
 
     prevent_quit();
-
     loop {
         clear_background(BLACK);
-
-        // Process keys, mouse etc.
         render_ctx.update();
 
         //TODO signal handling
 
-        if render_ctx.should_exit { break; }
-
+        if render_ctx.should_exit {
+            break;
+        }
         render_ctx.draw();
         next_frame().await
     }
@@ -86,17 +71,17 @@ struct RenderContext {
     //struct State {
     widgets: BTreeMap<WdgID, (String, WdgID)>, //TODO add Vec<messages> to every widget
     resources: BTreeMap<ResID, String>,
-    objects: BTreeMap<ObjID, ((ObjXY,f64), ResID)>,
+    objects: BTreeMap<ObjID, ((ObjXY, f64), ResID)>,
     hero: (ObjXY, f64),
     hf_x: f32,
-    hf_y:f32,
+    hf_y: f32,
     login: String,
     name: String,
     timestamp: String,
     //}
     //TODO
     //struct RenderState {
-    tile_colors: BTreeMap<String,[u8;4]>,
+    tile_colors: BTreeMap<String, [u8; 4]>,
     palette: [[u8; 4]; 256],
     grids_tiles: Vec<(i32, i32, macroquad::texture::Texture2D)>,
     // }
@@ -116,8 +101,7 @@ struct RenderContext {
 }
 
 impl RenderContext {
-    fn new (event_tx: Sender<driver::Event>, event_rx: Receiver<state::Event>) -> RenderContext {
-
+    fn new(event_tx: Sender<driver::Event>, event_rx: Receiver<state::Event>) -> RenderContext {
         //#[cfg(feature = "dump_events")]
         //let mut dumper = dumper::Dumper::init().expect("unable to create dumper");
 
@@ -131,13 +115,13 @@ impl RenderContext {
             hf_x: 0.0,
             hf_y: 0.0,
             login: "nobody".into(), //FIXME set to login on login
-            name: "noone".into(), //FIXME set to hero name on hero choise
+            name: "noone".into(),   //FIXME set to hero name on hero choise
             timestamp: chrono::Local::now().format("%Y-%m-%d %H-%M-%S").to_string(),
             tile_colors: {
                 let f = File::open("tile_colors.ron").expect("unable to open tile_colors.ron");
                 from_reader(BufReader::new(f)).expect("unable to deserialize")
             },
-            palette: [[0,0,250,255]; 256],
+            palette: [[0, 0, 250, 255]; 256],
             grids_tiles: Vec::new(),
             show_tiles: true,
             show_heights: true,
@@ -152,21 +136,21 @@ impl RenderContext {
         }
     }
 
-    fn tiles_to_png (&self, surface: &Surface) -> Result<()> {
+    fn tiles_to_png(&self, surface: &Surface) -> Result<()> {
         match surface.tiles() {
             Some(ref tiles) => {
                 //FIXME remove expect(), propagate error up
                 util::tiles_to_png(&self.login, &self.name, &self.timestamp, surface.x(), surface.y(), tiles /* TODO &s.z */).expect("Unable to save tiles");
                 Ok(())
-            },
+            }
             None => {
                 warn!("surface is tileless");
                 Ok(())
-            },
+            }
         }
     }
 
-    fn event (&mut self, event: state::Event) {
+    fn event(&mut self, event: state::Event) {
         /* TODO app.event(event) */
         match event {
             /*state::Event::Tiles(tiles) => {
@@ -204,35 +188,33 @@ impl RenderContext {
                     self.grids_tiles.push((surface.x(), surface.y(), texture));
                 }
                 #[cfg(TODO)]
-                let owning = ObjTex::plane_from_owning(1100.0, x, y, ol.as_ref())
-                    .bake(self.main_color.clone(), &mut self.factory);
+                let owning = ObjTex::plane_from_owning(1100.0, x, y, ol.as_ref()).bake(self.main_color.clone(), &mut self.factory);
                 #[cfg(TODO)]
                 self.grids_owning.push(owning);
                 //let heights = ObjCol::grid_from_heights(100, 11.0, grid_x, grid_y, heights.as_ref(), 1.0)
                 //    .bake(main_color.clone(), &mut factory, threshold);
                 #[cfg(TODO)]
-                let heights = ObjCol::grid_from_heights2(100, 11.0, x, y, z.as_ref())
-                    .bake(self.main_color.clone(), &mut self.factory, self.threshold);
+                let heights = ObjCol::grid_from_heights2(100, 11.0, x, y, z.as_ref()).bake(self.main_color.clone(), &mut self.factory, self.threshold);
                 #[cfg(TODO)]
                 self.grids_heights.push(heights);
             }
-            state::Event::Obj(id,(xy,angle),resid) => {
+            state::Event::Obj(id, (xy, angle), resid) => {
                 debug!("RENDER: obj ({}, {}) {} {}", xy.0, xy.1, angle, resid);
                 //TODO ??? separate static objects like trees and
                 //dynamic objects like rabbits to two
                 //different caches
-                self.objects.insert(id, ((xy,angle), resid));
+                self.objects.insert(id, ((xy, angle), resid));
             }
             state::Event::ObjRemove(ref id) => {
                 debug!("RENDER: obj remove {}", id);
                 self.objects.remove(id);
             }
             state::Event::Hero(position) => {
-                debug!("RENDER: hero ({}, {}) {}", position.0.0, position.0.1, position.1);
+                debug!("RENDER: hero ({}, {}) {}", position.0 .0, position.0 .1, position.1);
                 //TODO ??? add to objects
                 self.hero = position;
                 if self.target_isnt_set {
-                    self.target = vec2(self.hero.0.0 as f32, self.hero.0.1 as f32);
+                    self.target = vec2(self.hero.0 .0 as f32, self.hero.0 .1 as f32);
                     self.target_isnt_set = false;
                 }
             }
@@ -240,9 +222,9 @@ impl RenderContext {
                 debug!("RENDER: res {} {}", id, name);
                 self.resources.insert(id, name);
             }
-            state::Event::Wdg(state::Wdg::New(id,name,parent)) => {
+            state::Event::Wdg(state::Wdg::New(id, name, parent)) => {
                 debug!("RENDER: wdg new {} {} {}", id, name, parent);
-                self.widgets.insert(id,(name.clone(),parent));
+                self.widgets.insert(id, (name.clone(), parent));
                 //self.xui.add_widget(id,name,parent).expect("unable to ui.add_widget");
             }
             state::Event::Wdg(state::Wdg::Msg(id, name, _args)) => {
@@ -254,7 +236,7 @@ impl RenderContext {
                 self.widgets.remove(&id);
                 //self.xui.del_widget(id).expect("unable to ui.del_widget");
             }
-            state::Event::Hearthfire(ObjXY(x,y)) => {
+            state::Event::Hearthfire(ObjXY(x, y)) => {
                 debug!("RENDER: hearthfire ({}, {})", x, y);
                 self.hf_x = x as f32;
                 self.hf_y = y as f32;
@@ -262,7 +244,7 @@ impl RenderContext {
         }
     }
 
-    fn update (&mut self) {
+    fn update(&mut self) {
         self.zoom *= 1.0 + mouse_wheel().1 / 10.0;
 
         let mouse = Vec2::from(mouse_position());
@@ -295,7 +277,9 @@ impl RenderContext {
                     //dumper.dump(&event).expect("unable to dump event");
                     self.event(event);
                 }
-                Err(Empty) => { break; }
+                Err(Empty) => {
+                    break;
+                }
                 Err(Disconnected) => {
                     info!("render: disconnected from que");
                     self.should_exit = true;
@@ -314,7 +298,7 @@ impl RenderContext {
                     for (_, resid) in self.objects.values() {
                         *objects_by_resid.entry(*resid).or_default() += 1;
                     }
-                    for (resid,quantity) in objects_by_resid.iter() {
+                    for (resid, quantity) in objects_by_resid.iter() {
                         ui.horizontal(|ui| {
                             if let Some(resname) = self.resources.get(resid) {
                                 ui.label(resname);
@@ -330,7 +314,7 @@ impl RenderContext {
         });
     }
 
-    fn draw (&self) {
+    fn draw(&self) {
         set_camera(&self.camera);
         self.draw_tiles();
         self.draw_owning();
@@ -342,7 +326,7 @@ impl RenderContext {
         self.draw_gui();
     }
 
-    fn draw_tiles (&self) {
+    fn draw_tiles(&self) {
         //TODO self.config.show_tiles: RenderConfig
         if self.show_tiles {
             for &(x, y, texture) in self.grids_tiles.iter() {
@@ -351,31 +335,33 @@ impl RenderContext {
                 //debug!("RENDER: draw texture at ({}, {})", x, y);
                 let params = DrawTextureParams {
                     dest_size: Some(Vec2::new(1100.0, 1100.0)),
-                    .. Default::default()
+                    ..Default::default()
                 };
                 macroquad::texture::draw_texture_ex(texture, x, y, WHITE, params);
             }
         }
     }
 
-    fn draw_owning (&self) {
+    fn draw_owning(&self) {
         if self.show_owning {
-            #[cfg(TODO)] for t in self.state.grids_owning.iter() {
+            #[cfg(TODO)]
+            for t in self.state.grids_owning.iter() {
                 self.state.encoder.draw(&t.slice, &self.state.pso_tex, &t.data);
             }
         }
     }
 
-    fn draw_heights (&self) {
+    fn draw_heights(&self) {
         if self.show_heights {
-            #[cfg(TODO)] for t in self.state.grids_heights.iter() {
+            #[cfg(TODO)]
+            for t in self.state.grids_heights.iter() {
                 self.state.encoder.draw(&t.slice, &self.state.pso_col, &t.data);
             }
         }
     }
 
-    fn draw_objects (&self) {
-        for ((ObjXY(x,y),angle),_) in self.objects.values() {
+    fn draw_objects(&self) {
+        for ((ObjXY(x, y), angle), _) in self.objects.values() {
             const OBJ_SIZE: f64 = 4.0;
             {
                 let x = (x + -OBJ_SIZE / 2.0) as f32;
@@ -388,34 +374,34 @@ impl RenderContext {
         }
     }
 
-    fn draw_marks (&self) {
+    fn draw_marks(&self) {
         use macroquad::prelude::*;
         for mark in &self.marks {
             const OBJ_SIZE: f32 = 2.0;
-            let x = mark[0] + - OBJ_SIZE / 2.0;
-            let y = mark[1] + - OBJ_SIZE / 2.0;
+            let x = mark[0] + -OBJ_SIZE / 2.0;
+            let y = mark[1] + -OBJ_SIZE / 2.0;
             let w = OBJ_SIZE;
             let h = OBJ_SIZE;
             draw_rectangle(x, y, w, h, RED);
         }
     }
 
-    fn draw_hero (&self) {
+    fn draw_hero(&self) {
         const OBJ_SIZE: f32 = 4.0;
-        let x = self.hero.0.0 as f32 - OBJ_SIZE / 2.0;
-        let y = self.hero.0.1 as f32 - OBJ_SIZE / 2.0;
+        let x = self.hero.0 .0 as f32 - OBJ_SIZE / 2.0;
+        let y = self.hero.0 .1 as f32 - OBJ_SIZE / 2.0;
         let w = OBJ_SIZE;
         let h = OBJ_SIZE;
         macroquad::shapes::draw_rectangle(x, y, w, h, BLUE);
-        direction_marker(self.hero.0.0 as f32, self.hero.0.1 as f32, self.hero.1 as f32, OBJ_SIZE as f32, BLUE);
+        direction_marker(self.hero.0 .0 as f32, self.hero.0 .1 as f32, self.hero.1 as f32, OBJ_SIZE as f32, BLUE);
     }
 
-    fn draw_gui (&self) {
+    fn draw_gui(&self) {
         egui_macroquad::draw();
     }
 }
 
-fn direction_marker (x: f32, y: f32, angle: f32, size: f32, color: Color) {
+fn direction_marker(x: f32, y: f32, angle: f32, size: f32, color: Color) {
     let rot = nalgebra::Rotation2::new(angle);
     //TODO let obj = nalgebra::geometry::Translation::from([x, y]);
     let p0 = rot * (nalgebra::Point2::new(1.0, 0.5) * size);
